732468663947e4aceca5daf6c57f8dc6053f860c
hunnerrose <114459778+hunnerrose@users.noreply.github.com>
2023-04-18 17:46:12 -0700

    generated static build files and added middleware for node server to run build files

--- anime_son/server/server.js
+++ anime_son/server/server.js
@@ -10,6 +10,8 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+    //focus on hosting our node server and in turn, allowing our node server to give us the static files of our react app. in terminal run 'npm run build'. This minifies and optimizes our react app down into a build folder with a bunch of static files. Now we instruct our node server to serve up this build folder vvvv
+app.use(express.static(path.join(__dirname, '../build')));
 
 //CONTROLLERS
 const animesController = require('./controllers/animes_controller');
@@ -18,4 +20,8 @@ app.use('/api/animes', animesController);
 //LISTEN
 app.listen(4005, () => {
     console.log('Server is running on port 4005');
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
 })
